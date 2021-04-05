2e56fdb85584e7065ddeb6c787f6189078875539
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2021-04-05 22:29:57 +0100

    rudimentary en passant and promotion
    encountering a bug where player gets extra move when king takes checking piece

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ SQ_SIZE = HEIGHT/DIMENSION
 white_pieces = [1,2,3,4,5,6]
 black_pieces = [7,8,9,10,11,12]
 white_to_move = True
+bob = []
 
 # TO DO
-# castling and en passant 
+# castling 
 #move log - see 2 
 #sometimes after one player moves it gets stuck. (if you click too many times)
 	#after player whos turn it isnt tries to take? not limited to this
 
-#getting checks completed for black
-#king shoudnt be able to move into checkx
+#king gets extra move when taking checking piece 
+
 
 def load_images():
 	temp = {}
@@ -43,7 +44,8 @@ def main():
 	chess_board = np.array([[0 for x in range(8)] for y in range(8)])
 	chess_board = starting_position(chess_board)
 #	valid_moves = get_all_moves(chess_board)
-	valid_moves = resolve_checks(chess_board)
+	en_Passant = []
+	valid_moves = resolve_checks(chess_board,en_Passant)
 
 #	print('/n')
 #	print(valid_moves)
@@ -64,7 +66,7 @@ def main():
 				row = location[1]//SQ_SIZE
 				sq_clicked = (row,col)
 				sqs_clicked.append(sq_clicked)
-				print(sqs_clicked[0])
+	#			print(sqs_clicked[0])
 				if len(sqs_clicked) == 2:
 					if sqs_clicked[0] == sqs_clicked[1]: #if user clicks same square
 						pass
@@ -73,7 +75,7 @@ def main():
 							chess_board[int(sqs_clicked[0][0])][int(sqs_clicked[0][1])] in black_pieces):
 							for item in valid_moves:
 								if item[0] == (int(sqs_clicked[0][0]),int(sqs_clicked[0][1])) and item[1] == (int(sqs_clicked[1][0]),int(sqs_clicked[1][1])):
-									chess_board = move_piece(sqs_clicked[0], sqs_clicked[1], chess_board)
+									chess_board, enPassant = move_piece(sqs_clicked[0], sqs_clicked[1], chess_board,en_Passant)
 									move_made = True
 									white_to_move =  not white_to_move
 									print('Whites Move') if white_to_move is True else print('Blacks Move')
@@ -84,21 +86,26 @@ def main():
 								else: 
 									pass
 							display_image(images, window, chess_board)
-						sq_clicked = ()
-						sqs_clicked = []
 
+						else:
+							pass
+					sq_clicked = ()
+					sqs_clicked = []
+					print('Empty')
+					print(sqs_clicked)
 				else:
 					pass
 		if move_made:
 #			valid_moves = get_all_moves(chess_board)
-			valid_moves = resolve_checks(chess_board)
+			valid_moves = resolve_checks(chess_board,enPassant)
+	#		print(valid_moves)
 		draw_board(window)
 		display_image(images,window,chess_board)
 		clock.tick(15)
 		p.display.flip()	
 	return
 
-def get_all_moves(board,pins): 
+def get_all_moves(board,pins,en_Passant): 
 	counter = 0
 	moves = []
 	hard_moves = [[(6,4),(4,4)]]
@@ -106,7 +113,7 @@ def get_all_moves(board,pins):
 		for j in range(len(board[i])):
 
 			if board[i][j] == 1 or board[i][j] == 7: #get all pawn moves
-				moves = pawn_moves(moves, i,j,board,pins)
+				moves = pawn_moves(moves, i,j,board,pins,en_Passant)
 				pass
 			elif board[i][j] == 2 or board[i][j] == 8: #get all knight moves 
 				moves = knight_moves(moves, i,j,board,pins)
@@ -176,7 +183,9 @@ def get_valid_moves(board, x=0,y=0):
 					elif white_to_move is False and board[row][column] in white_pieces or white_to_move is True and board[row][column] in black_pieces: # if white piece, and 
 						enemy_counter = enemy_counter + 1 
 					#	print('Theres a check in town')
-						if (6<=j<=7) and i == 1 and (board[row][column] == 1 or board[row][column] == 7):
+						if (i == 1) and ((board[row][column] == 1 and (6<=j<=7)) or (board[row][column] == 7 and (4<=j<=5))):
+							print('its meee')
+							print(i)
 							if ally_counter == 0: # add conditions in which pawn is checking king
 								checks.append((row,column,direction[0],direction[1]))
 							elif ally_counter == 1:
@@ -198,7 +207,6 @@ def get_valid_moves(board, x=0,y=0):
 								pins.append(possible_pin)
 						else:
 							break
-
 			else:
 				break
 
@@ -208,14 +216,12 @@ def get_valid_moves(board, x=0,y=0):
 			if white_to_move is False and board[king_row+move[0]][king_col+move[1]] == 2:
 				checks.append((king_row+move[0],king_col+move[1]))
 
-	print(checks)
-#	print('heyyyyyyyyyyyyyyyy')
 	return checks,pins,king_row,king_col
 
 
-def resolve_checks(board): #checkmate calculated here
+def resolve_checks(board,en_Passant): #checkmate calculated here
 	checks, pins,king_row,king_col = get_valid_moves(board)
-	moves = get_all_moves(board,pins)
+	moves = get_all_moves(board,pins,en_Passant)
 	valid_squares = []
 
 	if len(checks) == 0:
@@ -236,22 +242,34 @@ def resolve_checks(board): #checkmate calculated here
 			#	print('hi')
 				valid_square = (king_row + check[2]*i, king_col + check[3]*i) #search in direction of check until attacking piece is reached 
 				valid_squares.append(valid_square)
-			#	print(valid_squares)
+#				print(valid_squares)
 				if valid_square == (check_row,check_col):
 					break
+				if board[check_row][check_col] == 1 or board[check_row][check_col] == 7:
+					print('hey')
+					break
 		moves = [move for move in moves if move[1] in valid_squares]
-		moves = king_moves(moves,king_row,king_col,board)			
+		moves = king_moves(moves,king_row,king_col,board)		
 
 	elif len(checks) == 2:
 		moves = king_moves(moves,king_row,king_col,board) 
 		
 	return moves
-	#PINS
-	#cant move pinned piece, out of pin, can move up and down pin 
 
-def move_piece(old_square, new_square, board):
+def move_piece(old_square, new_square, board,en_Passant):
+	#print(en_Passant)
+	global bob
 	old_row,old_col,new_row,new_col = old_square[0], old_square[1],new_square[0],new_square[1]
 	old_row, old_col,new_row,new_col  = int(old_row), int(old_col),int(new_row),int(new_col)
+	board = en_passant_take(old_row, old_col,new_row, new_col,bob,board)
+#	bob = []
+	en_Passant = check_en_Passant(old_row, old_col,new_row,new_col,board) 
+	board = check_promotion(old_row,old_col,new_row,new_col,board)
+	
+	bob = en_Passant
+#	print(bob)
+
+#	print(en_Passant)
 	temp = board[old_row:old_row+1, old_col:old_col+1]
 	if temp == 0: 
 		pass
@@ -259,7 +277,52 @@ def move_piece(old_square, new_square, board):
 		temp = int(temp)
 		board[new_row:new_row+1, new_col:new_col+1] = temp
 		board[old_row:old_row+1, old_col:old_col+1] = 0
+	return board, en_Passant
+
+def check_en_Passant(old_row, old_col,new_row,new_col,board):
+	en_passant = []
+	if white_to_move is False and board[old_row][old_col] == 7:
+		if old_row == 1 and new_row == 3:
+			en_passant.append((new_row, new_col))
+		else:
+			pass
+	elif white_to_move is True and board[old_row][old_col] == 1:
+		if old_row == 6 and new_row == 4:
+			en_passant.append((new_row, new_col))
+		else:
+			pass
+	else:
+		pass 
+	return en_passant 
+
+def en_passant_take(old_r,old_c ,r,c,en_Passant,board):
+#	print(en_Passant)
+	#if move made is an en_passant move then the en_passant square should be taken
+	#if white to move is True and (new_row - old_row == -1) and
+	# print(en_Passant)
+	# print(old_r,c-1)
+	if (white_to_move is True) and (len(en_Passant)!= 0) and (r - old_r == -1) and (((old_r,old_c-1) in en_Passant) or ((old_r,old_c+1) in en_Passant)) and (old_c - c == -1 or old_c - c == 1):
+		board[en_Passant[0][0]][en_Passant[0][1]] = 0
+	elif (white_to_move is False) and (len(en_Passant)!= 0) and (r - old_r == 1) and (((old_r,old_c-1) in en_Passant) or ((old_r,old_c+1) in en_Passant)) and (old_c - c == -1 or old_c - c == 1):
+		board[en_Passant[0][0]][en_Passant[0][1]] = 0
+
 	return board
+
+def check_promotion(old_r,old_c,r,c,board):
+	# print('************************')
+	# print(old_r)
+	# print(old_c)
+	# print(r)
+	# print('************************')
+	if white_to_move is True and board[old_r][old_c] == 1 and r == 0:
+		board[old_r][old_c] = 5
+	elif white_to_move is False and board[old_r][old_c] == 7 and r == 7:
+		board[old_r][old_c] = 11
+	else:
+		pass
+
+	return board
+
 
 def draw_board(window): #some of this should go in main 
 	colour1 = (235, 235, 208)
@@ -277,7 +340,7 @@ def display_image(images, window,chess_board):
 	      	if piece != 0:
 		      	window.blit(images[piece], p.Rect(j*SQ_SIZE, i*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
-def pawn_moves(moves,r,c,board,pins): #add take functionality
+def pawn_moves(moves,r,c,board,pins,en_Passant): #add take functionality
 
  #if pawn is pinned, pawnPinned = True 
  #if true, can only move in direction of pin and cant take 
@@ -324,6 +387,10 @@ def pawn_moves(moves,r,c,board,pins): #add take functionality
 			moves.append([(r,c),(r-1,c+1)])
 		else:
 			pass
+		if white_to_move is True and  r == 3 and (r,c-1)in en_Passant: 
+			moves.append([(r,c),(r-1,c-1)])
+		elif white_to_move is True and  r == 3 and (r,c+1) in en_Passant: 
+			moves.append([(r,c),(r-1,c+1)])
 
 		if white_to_move is False and  r != 7 and board[r][c] == 7 and board[r+1][c-1] in white_pieces: #take white
 			moves.append([(r,c),(r+1,c-1)])
@@ -333,6 +400,10 @@ def pawn_moves(moves,r,c,board,pins): #add take functionality
 			moves.append([(r,c),(r+1,c+1)])
 		else:
 			pass
+		if white_to_move is False and  r == 4  and (r,c-1)in en_Passant: 
+			moves.append([(r,c),(r+1,c-1)])
+		elif white_to_move is False and  r == 4 and (r,c+1) in en_Passant: 
+			moves.append([(r,c),(r+1,c+1)])
 	return moves
 
 def knight_moves(moves,r,c,board,pins): #could be done in one for loop
@@ -415,7 +486,6 @@ def get_diagonal_moves(moves,r,c,x,y,board,stopper):
 			pass
 		if board[x][y] != 0:
 			stopper = False
-		
 	return moves, stopper
 		
 def rook_moves(moves,r,c,board,pins):
@@ -478,15 +548,13 @@ def king_moves(moves,r,c,board): #is a loop more efficient
 	return moves
 
 def get_king_moves(moves,r,c,x,y,board):
-
 	if (0 <= x <= 7) and (0 <= y <= 7):
 		if (board[x][y] == 0) or (board[r][c] == 6 and board[x][y] in black_pieces) or (board[r][c] == 12 and board[x][y] in white_pieces):
 			checks, pins, king_row, king_col = get_valid_moves(board,x,y)
-			if len(checks) == 0:
+			if len(checks) == 0: #logic needs filling 
 				moves.append([(r,c),(x,y)])
 		else:
 			pass
-
 	return moves
 
 if __name__ == "__main__" :
